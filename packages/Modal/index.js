c9b93c1cbaaa844a5e302b68615cb71de841ef7b
ericyip <letitbemine@gmail.com>
2018-04-07 02:59:20 +0800

    Add basic modal layout

--- packages/Modal/index.js
+++ packages/Modal/index.js
@@ -3,14 +3,42 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Root = styled.div``;
+const Root = styled.div`
+`;
 
-class Modal extends Component {
+const Layer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 500;
+  background: rgba(9, 30, 66, 0.54);
+`;
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  background-color: ${props => props.backgroundColor ? props.backgroundColor : '#fff'};
+  width: ${props => props.width ? props.width : '50%'};
+  height: 500px;
+  z-index: 510;
+`;
+
+type Props = {
+  width?: string,
+  onClose?: () => mixed,
+}
+
+class Modal extends Component<Props> {
   state = {  }
   render() {
+    const { width } = this.props;
     return (
       <Root>
-        yoyo
+        <Layer />
+        <ModalWrapper width={width}>
+          yoyo
+        </ModalWrapper>
       </Root>
     );
   }
